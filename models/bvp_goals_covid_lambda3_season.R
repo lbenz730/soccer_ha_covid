432b065cc32e9adab152af1665a876541e61214a
lbenz730 <luke.benz@yale.edu>
2020-12-01 11:32:43 -0500

    untrack files in .gitignore

--- models/bvp_goals_covid_lambda3_season.R
+++ models/bvp_goals_covid_lambda3_season.R
@@ -25,65 +25,67 @@ for(i in 1:nrow(league_info)) {
   df <- read_leage_csvs(league) %>% 
     filter(!is.na(home_score), !is.na(away_score))
   
-  ### In the Bundesliga, the last 2 games of the seaon is a promotion/regaltion game
-  ### between 3rd place in 2nd division and 3rd from last in top division. Filtering out for now
-  ### It's likely this is the ccould be the case in other leagues but not worrying as much about that now
-  if(league == "German Bundesliga") {
-    df <-
-      group_by(df, season) %>%
-      mutate('game_id' = 1:n()) %>%
-      filter(game_id < max(game_id) - 1) %>%
-      ungroup()
-  }
-  
-  df %>% 
+  ### Filter Out Games for relegation playoffs
+  keep <- 
+    df %>% 
     select(home, away, season) %>% 
-    pivot_longer(c('home', 'away')) %>% 
-    group_by(value, season) %>% 
+    pivot_longer(c('home', 'away'),
+                 values_to = 'team') %>% 
+    group_by(team, season) %>% 
     count() %>% 
     ungroup() %>% 
-    view()
+    filter(n > 3) 
+  
+  if(nrow(keep) < n_distinct(c(paste0(df$home, df$season), paste0(df$away, df$season)))) {
     
-  
-  ### Team IDs
-  covid_date <- as.Date(league_info$restart_date[i], '%m/%d/%y')
-  df <- 
-    df %>% 
-    mutate('season' = as.character(season)) %>% 
-    mutate('home' = paste(home, season, sep = '_'),
-           'away' = paste(away, season, sep = '_')) %>% 
-    mutate('season_numeric' = as.numeric(as.factor(season)))
-  team_ids <- team_codes(df)
-  df <- 
-    select(df, home, away, home_score, away_score, season, date, season_numeric) %>% 
-    mutate('home_id' = team_ids[home],
-           'away_id' = team_ids[away],
-           'pre_covid' = as.numeric(date < covid_date))
-  
-  ### List of Stan Params
-  stan_data <- list(
-    num_clubs = length(team_ids),
-    num_games = nrow(df),
-    num_seasons = n_distinct(df$season_numeric),
-    home_team_code = df$home_id,
-    away_team_code = df$away_id,
-    h_goals = df$home_score,
-    a_goals = df$away_score,
-    ind_pre = df$pre_covid,
-    season = df$season_numeric
-  )
-  
-  ### Fit Model
-  model <- stan(file = here('stan/bvp_goals_covid_lambda3_season.stan'), 
-                data = stan_data, 
-                seed = 73097,
-                chains = 3, 
-                iter = 3 * 7000, 
-                warmup = 3 * 2000, 
-                control = list(adapt_delta = 0.95))
-  
-  ### Save Model and Posterior
-  write_rds(model, here(paste0(glue('model_objects/{directory}/'), gsub("\\s", "_", tolower(league)), '.rds')))
-  posterior <- extract(model)
-  write_rds(posterior, here(paste0(glue('posteriors/{directory}/'), gsub("\\s", "_", tolower(league)), '.rds')))
+    df <- 
+      df %>% 
+      semi_join(keep, by = c('home' = 'team', 'season' = 'season')) %>% 
+      semi_join(keep, by = c('away' = 'team', 'season' = 'season'))
+    
+    
+    
+    
+    ### Team IDs
+    covid_date <- as.Date(league_info$restart_date[i], '%m/%d/%y')
+    df <- 
+      df %>% 
+      mutate('season' = as.character(season)) %>% 
+      mutate('home' = paste(home, season, sep = '_'),
+             'away' = paste(away, season, sep = '_')) %>% 
+      mutate('season_numeric' = as.numeric(as.factor(season)))
+    team_ids <- team_codes(df)
+    df <- 
+      select(df, home, away, home_score, away_score, season, date, season_numeric) %>% 
+      mutate('home_id' = team_ids[home],
+             'away_id' = team_ids[away],
+             'pre_covid' = as.numeric(date < covid_date))
+    
+    ### List of Stan Params
+    stan_data <- list(
+      num_clubs = length(team_ids),
+      num_games = nrow(df),
+      num_seasons = n_distinct(df$season_numeric),
+      home_team_code = df$home_id,
+      away_team_code = df$away_id,
+      h_goals = df$home_score,
+      a_goals = df$away_score,
+      ind_pre = df$pre_covid,
+      season = df$season_numeric
+    )
+    
+    ### Fit Model
+    model <- stan(file = here('stan/bvp_goals_covid_lambda3_season.stan'), 
+                  data = stan_data, 
+                  seed = 73097,
+                  chains = 3, 
+                  iter = ifelse(i == 2, 3, 1) *  7000, 
+                  warmup = ifelse(i == 2, 3, 1) * 2000, 
+                  control = list(adapt_delta = 0.95))
+    
+    ### Save Model and Posterior
+    write_rds(model, here(paste0(glue('model_objects/{directory}/'), gsub("\\s", "_", tolower(league)), '.rds')))
+    posterior <- extract(model)
+    write_rds(posterior, here(paste0(glue('posteriors/{directory}/'), gsub("\\s", "_", tolower(league)), '.rds')))
+  }
 }
